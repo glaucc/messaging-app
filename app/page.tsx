0530f3c6b77d8f49a3ca81e9ea18ed643a4712d6
glaucc <ahmedovyusif8785@gmail.com>
2023-10-23 00:59:46 +0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+    <p className='text-3xl font-bold text-indigo-500'>test</p>
+    <Button>Click</Button>
+    </div>
+  )
+}
