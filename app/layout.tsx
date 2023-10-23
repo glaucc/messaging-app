b741b084fc2b1227fcde61fa725ef75cdc4eff85
glaucc <ahmedovyusif8785@gmail.com>
2023-10-23 19:15:34 +0400

    auth add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
 import './globals.css'
+
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -15,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
