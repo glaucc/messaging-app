c22738d2a4a4d79d3de560cebd057676140fc246
glaucc <ahmedovyusif8785@gmail.com>
2023-10-26 17:39:11 +0400

    file upload error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
+import { withUt } from "uploadthing/tw";
 import type { Config } from 'tailwindcss'
+
+
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{ts,tsx,mdx}",
   ],
   theme: {
     extend: {
