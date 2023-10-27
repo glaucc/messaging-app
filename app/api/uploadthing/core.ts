d395d234ef3aba5c5c8adcdc5bf6d6c0ba01efcf
glaucc <ahmedovyusif8785@gmail.com>
2023-10-27 21:09:03 +0400

    error solve trying

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -4,16 +4,16 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
  
 const handleAuth = () => {
-    const { userId } = auth();
-    if (!userId) throw new Error("Unauthorized");
-    return { userId: userId };
+  const { userId } = auth();
+  if (!userId) throw new Error("Unauthorized");
+  return { userId: userId };
 }
 
 export const ourFileRouter = {
-    serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1} })
+  serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
-    messageFile: f(["image", "pdf"])
+  messageFile: f(["image", "pdf"])
     .middleware(() => handleAuth())
     .onUploadComplete(() => {})
 } satisfies FileRouter;
--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
-'use client';
 
 import { UploadDropzone } from "@/lib/uploadthing";
+// import { UploadDropzone } from "@uploadthing/react";
 
 import "@uploadthing/react/styles.css"
 
@@ -19,7 +19,7 @@ export const FileUpload = ({
         <UploadDropzone 
             endpoint={endpoint}
             onClientUploadComplete={(res) => {
-                onChange(res?.[0].fileUrl);
+                onChange(res?.[0].url);
             }}
             onUploadError={(error: Error) => {
                 console.log(error);
