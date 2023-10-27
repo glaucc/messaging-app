9a394d30b0ddcab42aca7359adb2b0081a79167c
glaucc <ahmedovyusif8785@gmail.com>
2023-10-27 22:16:31 +0400

    form

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as z from 'zod';
+import axios from "axios";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
@@ -25,6 +26,7 @@ import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { useEffect, useState } from 'react';
 import { FileUpload } from '../file-upload';
+import { useRouter } from 'next/navigation';
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -37,6 +39,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false);
+
+    const router = useRouter();
 
     useEffect(() => {
         setIsMounted(true);
@@ -53,7 +57,15 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post("/api/servers", values);
+
+            form.reset();
+            router.refresh();
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     if (!isMounted) {
