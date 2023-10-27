aaa9577d908fc4466dad0cc6633af235e36fb9fd
glaucc <ahmedovyusif8785@gmail.com>
2023-10-27 21:52:15 +0400

    x button and error fix

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -3,6 +3,8 @@ import { UploadDropzone } from "@/lib/uploadthing";
 // import { UploadDropzone } from "@uploadthing/react";
 
 import "@uploadthing/react/styles.css"
+import { X } from "lucide-react";
+import Image from "next/image";
 
 interface FileUploadProps {
     onChange: (url?: string) => void;
@@ -15,6 +17,26 @@ export const FileUpload = ({
     value,
     endpoint
 }: FileUploadProps) => {
+    const fileType = value?.split('.').pop();
+
+    if (value && fileType !== "pdf") {
+        return (
+            <div className="relative h-20 w-20">
+                <Image 
+                    fill
+                    src={value}
+                    alt="Upload"
+                    className="rounded-full"
+                />
+                <button 
+                onClick={() => onChange("")}
+                className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+                >
+                    <X />
+                </button>
+            </div>
+        )
+    }
     return (
         <UploadDropzone 
             endpoint={endpoint}
