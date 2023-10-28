79dad173370d85882688c2a569ec30595ba061be
glaucc <ahmedovyusif8785@gmail.com>
2023-10-28 17:49:33 +0400

    navigation sidebar add a server

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,32 +1,6 @@
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
-
-import { redirect } from "next/navigation";
-
-const ServerIdPage = async () => {
-    const profile = await currentProfile();
-
-    if (!profile) {
-        return redirect("/")
-    }
-
-
-    const servers = await db.server.findMany({
-        where: {
-            members: {
-                some: {
-                    profileId: profile.id
-                }
-            }
-        }
-    });
-
-
-    
+const ServerIdPage = () => {
     return ( 
-        <div
-            className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3"
-        >Server Id Page</div>
+        <div>Server Id Page</div>
      );
 }
  
