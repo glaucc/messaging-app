33184829d7ea97da3c1d8f7ec36313666971f3bd
glaucc <ahmedovyusif8785@gmail.com>
2023-10-29 02:47:04 +0400

    serverheader

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { ServerHeader } from "./server-header";
 
 interface ServerSidebarProps {
     serverId: string;
@@ -41,8 +42,20 @@ export const ServerSidebar = async ({
     const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT)
     const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO)
     const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO)
+    const members = server?.members.filter((member) => member.profileId !== profile.id)
+
+    if (!server) {
+        return redirect("/");
+    }
+
+    const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
     return (
-        <div>Server Sidebar Component</div>
+        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+            <ServerHeader 
+                server={server}
+                role={role}
+            />          
+        </div>
     )
 }
