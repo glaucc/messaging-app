7112054a456d6d831c0a3aacf9ff19463a043172
glaucc <ahmedovyusif8785@gmail.com>
2023-10-29 14:04:50 +0400

    invite modal and copy link

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -4,6 +4,7 @@ import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
     server: ServerWithMembersWithProfiles;
@@ -14,6 +15,8 @@ export const ServerHeader = ({
     server,
     role
 }: ServerHeaderProps) => {
+    const { onOpen } = useModal();
+
     const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -33,7 +36,9 @@ export const ServerHeader = ({
                     className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
                 >
                     {isModerator && (
-                        <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+                        <DropdownMenuItem 
+                            onClick={() => onOpen("invite", { server })}
+                            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
                             Invite People
                             <UserPlus className="h-4 w-4 ml-auto" />
                         </DropdownMenuItem>
