b9b645130afd5ac79e55f9568e6e11ebb9d71bc0
glaucc <ahmedovyusif8785@gmail.com>
2023-10-30 22:07:49 +0400

    manage members 2

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -12,12 +12,31 @@ import {
 
 import { useModal } from '@/hooks/use-modal-store';
 import { ServerWithMembersWithProfiles } from '@/types';
+import { ScrollArea } from '../ui/scroll-area';
+import { UserAvatar } from '../user-avatar';
+import { ShieldAlert, ShieldCheck } from 'lucide-react';
+import { useState } from 'react';
+import { 
+    DropdownMenu, 
+    DropdownMenuContent, 
+    DropdownMenuItem, 
+    DropdownMenuPortal, 
+    DropdownMenuSeparator, 
+    DropdownMenuSub, 
+    DropdownMenuSubContent, 
+    DropdownMenuSubTrigger, 
+    DropdownMenuTrigger 
+} from '../ui/dropdown-menu';
 
-
+const roleIconMap = {
+    "GUEST": null,
+    "MODERATOR": <ShieldCheck className='h-4 w-4 ml-2 text-indigo-500' />,
+    "ADMIN": <ShieldAlert className='h-4 w-4 text-rose-500' />
+}
 
 export const MembersModal = () => {
     const {onOpen, isOpen, onClose, type, data } = useModal();
-
+    const [loadingId, setLoadingId] = useState("");
 
     const isModalOpen = isOpen && type === "members";
     const { server } = data as {server: ServerWithMembersWithProfiles};
@@ -27,18 +46,37 @@ export const MembersModal = () => {
     return (
     
         <Dialog open={isModalOpen} onOpenChange={onClose}>
-            <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+            <DialogContent className='bg-white text-black overflow-hidden'>
                 <DialogHeader className='pt-8 px-6'>
                     <DialogTitle className='text-2xl text-center font-bold'>
                         Manage Members
                     </DialogTitle>
-                </DialogHeader>
-                <DialogDescription>
+                <DialogDescription
+                    className='text-center text-zinc-500'
+                    >
                     {server?.members?.length} Members
                 </DialogDescription>
-                <div className='p-6'>
-                    hello members
-                </div>
+                </DialogHeader>
+                <ScrollArea className='mt-8 max-h-[420px] pr-6'>
+                    {server?.members?.map((member) => (
+                        <div key={member.id} className='flex items-center gap-x-2 mb-6'>
+                            <UserAvatar src={member.profile.imageUrl} />
+                            <div className='flex flex-col gap-y-1'>
+                                <div className='text-xs font-semibold flex items-center gap-x-1'>
+                                    {member.profile.name}
+                                    {roleIconMap[member.role]}
+                                </div>
+                                <p className='text-xs text-zinc-500'>
+                                    {member.profile.email}
+                                </p>
+                            </div>
+                            {server.profileId !== member.profileId && loadingId !== member.id && (
+                                <div className='ml-auto'>Actions!</div>
+
+                            )}
+                        </div>
+                    ))}
+                </ScrollArea>
             </DialogContent>
         </Dialog>
         
