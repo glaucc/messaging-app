0c3a3ce23f770bcdc18dd8e091ee9138654edb61
glaucc <ahmedovyusif8785@gmail.com>
2023-10-30 09:43:15 +0400

    generate a new link

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -16,10 +16,11 @@ import { Button } from '../ui/button';
 import { Check, Copy, RefreshCw } from 'lucide-react';
 import { useOrigin } from '@/hooks/use-origin';
 import { useState } from 'react';
+import axios from 'axios';
 
 
 export const InviteModal = () => {
-    const {isOpen, onClose, type, data } = useModal();
+    const {onOpen, isOpen, onClose, type, data } = useModal();
     const origin = useOrigin();
 
 
@@ -27,7 +28,7 @@ export const InviteModal = () => {
     const { server } = data;
 
     const [copied, setCopied] = useState(false);
-    const [loading, isLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
 
@@ -38,6 +39,19 @@ export const InviteModal = () => {
         setTimeout(() => {
             setCopied(false);
         }, 1000);
+    }
+
+    const onNew = async () => {
+        try {
+            setIsLoading(true);
+            const response = await axios.patch(`/api/servers/${server?.id}/invite-code`);
+
+            onOpen("invite", {server: response.data})
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -55,10 +69,11 @@ export const InviteModal = () => {
                     </Label>
                     <div className='flex items-center mt-2 gap-x-2'>
                         <Input 
+                        disabled={isLoading}
                             className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
                             value={inviteUrl}
                         />
-                        <Button onClick={onCopy} size="icon">
+                        <Button disabled={isLoading} onClick={onCopy} size="icon">
                             {copied 
                             ? <Check className='w-4 h-4' /> 
                             : <Copy className='w-4 h-4' />
@@ -66,6 +81,8 @@ export const InviteModal = () => {
                         </Button>
                     </div>
                     <Button
+                    onClick={onNew}
+                    disabled={isLoading}
                     variant="link"
                     size="sm"
                         className='text-xs text-zinc-500 mt-4'
