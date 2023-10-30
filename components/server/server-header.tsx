eb0064abe10ac377e3183c2ffd225f2c59448aaa
glaucc <ahmedovyusif8785@gmail.com>
2023-10-30 11:33:36 +0400

    managing members 1

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -54,7 +54,9 @@ export const ServerHeader = ({
                     )}
 
                     {isAdmin && (
-                        <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                        <DropdownMenuItem 
+                            onClick={() => onOpen("members", {server})}
+                            className="px-3 py-2 text-sm cursor-pointer">
                             Manage Members
                             <Users className="h-4 w-4 ml-auto" />
                         </DropdownMenuItem>
