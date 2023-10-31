77fb6deaf3bb86ca258857b8995807fd8d46546f
glaucc <ahmedovyusif8785@gmail.com>
2023-10-31 21:15:17 +0400

    manage members | kick

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -14,7 +14,7 @@ import { useModal } from '@/hooks/use-modal-store';
 import { ServerWithMembersWithProfiles } from '@/types';
 import { ScrollArea } from '../ui/scroll-area';
 import { UserAvatar } from '../user-avatar';
-import { ShieldAlert, ShieldCheck } from 'lucide-react';
+import { Check, Gavel, Loader2, MoreVertical, Shield, ShieldAlert, ShieldCheck, ShieldQuestion } from 'lucide-react';
 import { useState } from 'react';
 import { 
     DropdownMenu, 
@@ -71,8 +71,51 @@ export const MembersModal = () => {
                                 </p>
                             </div>
                             {server.profileId !== member.profileId && loadingId !== member.id && (
-                                <div className='ml-auto'>Actions!</div>
+                                <div className='ml-auto'>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger>
+                                            <MoreVertical className='h-4 w-4 text-zinc-500' />
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent side='left'>
+                                            <DropdownMenuSub>
+                                                <DropdownMenuSubTrigger className='flex items-center'>
+                                                    <ShieldQuestion className='w-4 h-4 mr-2' />
+                                                    <span>Role</span>
+                                                </DropdownMenuSubTrigger>
+                                                <DropdownMenuPortal>
+                                                    <DropdownMenuSubContent>
+                                                        
+                                                        <DropdownMenuItem>
+                                                            <Shield className='h-4 w-4 mr-2' />
+                                                            Guest
+                                                            {member.role === "GUEST" && (
+                                                                <Check className='h-4 w-4 ml-auto' />
+                                                            )}
+                                                        </DropdownMenuItem>
 
+                                                        <DropdownMenuItem>
+                                                            <ShieldCheck className='h-4 w-4 mr-2' />
+                                                            Moderator
+                                                            {member.role === "MODERATOR" && (
+                                                                <Check className='h-4 w-4 ml-auto' />
+                                                            )}
+                                                        </DropdownMenuItem>
+
+                                                    </DropdownMenuSubContent>
+                                                </DropdownMenuPortal>
+                                            </DropdownMenuSub>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>
+                                                <Gavel className='h-4 w-4 mr-2' />
+                                                Kick
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </div>
+
+                            )}
+                            {loadingId === member.id && (
+                                <Loader2 className='animate-spin text-zinc-500 ml-auto w-4 h-4' />
                             )}
                         </div>
                     ))}
